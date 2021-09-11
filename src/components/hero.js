939e8244a797b759ff87c29bcf5169ec6decab9d
Kevin Herrera <1herrerake@gmail.com>
2021-09-11 17:00:21 -0500

    home page update

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,14 @@
 import * as React from "react"
 
 const Hero = () => {
-  return <div>NOTHING</div>
+  return (
+    <section class="hero is-large is-primary">
+      <div class="hero-body has-text-centered">
+        <h1 class="title">Design.Create.Market.Sell</h1>
+        <h2 class="subtitle">Next Generation Online Commerce</h2>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
