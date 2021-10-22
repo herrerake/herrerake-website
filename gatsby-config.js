26061f67590b304e7430dc6b86a131a2f3fe2fed
Kevin Herrera <1herrerake@gmail.com>
2021-10-22 16:43:17 -0500

    back to what it was

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,10 +36,9 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        // apiURLOG: process.env.DEPLOY_URL
-        //   ? "https://herrerake-website.herokuapp.com"
-        //   : "http://localhost:1337",
-        apiURL: "http://localhost:1337",
+        apiURL: process.env.DEPLOY_URL
+          ? "https://herrerake-website.herokuapp.com"
+          : "http://localhost:1337",
         queryLimit: 1000, // Default to 100
         contentTypes: [`Users`, `Case-Studies`, `Portfolios`],
         //If using single types place them in this array.
