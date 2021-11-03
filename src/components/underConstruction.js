a436ecc6f792812bfe80c7601d5a8db187059611
Kevin Herrera <1herrerake@gmail.com>
2021-11-03 13:19:53 -0500

    made hero section and bottom navigation responsive for desktop. Cleaned up CSS for hero. removed console.logs.

--- src/components/underConstruction.js
+++ src/components/underConstruction.js
@@ -4,29 +4,24 @@ import { Link } from "gatsby"
 
 const UnderConstruction = () => {
   return (
-    <section className="section">
-      <div className="columns under-construction-hero">
-        <div>
-          <div className="column is-12-mobile">
-            <div className="mb-3">
-              <h1 className="has-text-white">Hustle sold separately</h1>
-              <h5 className="has-text-danger">stay stuned.</h5>
-            </div>
-            <div className="buttons">
-              <button className="button is-primary">
-                <Link className="is-capitalized remove-link-color" to="/">
-                  Home
-                </Link>
-              </button>
-              <button className="button is-primary">
-                <Link
-                  className="is-capitalized remove-link-color"
-                  to="/contact"
-                >
-                  Contact
-                </Link>
-              </button>
-            </div>
+    <section className="section under-construction-hero">
+      <div className="columns custom-hero-column">
+        <div className="column is-12-mobile">
+          <div className="mb-3">
+            <h1 className="has-text-white">Hustle sold separately</h1>
+            <h5 className="has-text-danger">stay stuned.</h5>
+          </div>
+          <div className="buttons">
+            <button className="button is-primary">
+              <Link className="is-capitalized remove-link-color" to="/">
+                Home
+              </Link>
+            </button>
+            <button className="button is-primary">
+              <Link className="is-capitalized remove-link-color" to="/contact">
+                Contact
+              </Link>
+            </button>
           </div>
         </div>
       </div>
