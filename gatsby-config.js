ba71d445c6299487d36f2aa6f4fc6a82320522dc
Kevin Herrera <1herrerake@gmail.com>
2021-11-03 00:32:43 -0500

    hail marry

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,9 +36,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: process.env.NODE_ENV === "production"
-          ? process.env.HEROKU_URL
-          : "http://localhost:1337",
+        apiURL: process.env.HEROKU_URL,
         queryLimit: 1000, // Default to 100
         contentTypes: [`Users`, `Case-Studies`, `Portfolios`],
         //If using single types place them in this array.
